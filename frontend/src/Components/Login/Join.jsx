46e4d6fc3f1928bb02565f2809fa26c26b506046
hong-sehyun <sese0733@naver.com>
2023-08-16 12:28:08 +0900

    0816

--- frontend/src/Components/Login/Join.jsx
+++ frontend/src/Components/Login/Join.jsx
@@ -99,7 +99,7 @@ const Join = () => {
                     </div>
                     {passwordMatch === null && <p>비밀번호를 입력해주세요.</p>}
                     {passwordMatch === false && <p className="error">비밀번호가 일치하지 않습니다.</p>}
-                    {passwordMatch === true && <p>비밀번호가 일치합니다. 회원가입을 해주세요.</p>}
+                    {passwordMatch === true && <p>비밀번호가 일치합니다.</p>}
                     <div></div>
                     <div>
                         <button type="submit">회원가입</button>
